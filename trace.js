5ead1d22726d171bbd2c4e003ba70f8a908ae146
Robin Lake <robin@tempus-ex.com>
2022-08-27 09:19:35 -0600

    new trace

--- trace.js
+++ trace.js
@@ -1,4 +1,12 @@
-const trace = label => value => {
+// inspect the contents of an element, with a label attached
+const traceWithLabel = label => value => {
   console.log(`${ label }: ${ value }`);
   return value;
 };
+
+// inspect the contents of an element
+const trace = (x) => {
+	console.log(x);
+	return x;
+};
+
